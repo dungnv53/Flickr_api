4292f158e4cd7ed1590a9320851325a5340a5ad6
dungnv53 <vandung_17@yahoo.com>
2015-01-19 00:42:05 +0700

    First kick baby

---
+++ README.md
@@ -0,0 +1,10 @@
+List of michalu.eu example codes
+==================
+
+* Scanning and generating barcodes using zxing library on Android ([here](http://michalu.eu/wordpress/scanning-and-generating-barcodes-using-zxing-library-on-android/)) 
+ 1. [__zxing-barcode-michalueu__](https://github.com/fockeRR/michalueu-examples/tree/master/zxing-barcode-michalueu)
+ 2. [__apklib zxinglib-michalu__](https://github.com/fockeRR/michalueu-examples/tree/master/zxinglib-michalu)
+* Flickr API - how to get images from Flickr by tag
+ 1. [__flickr-michalueu__](https://github.com/fockeRR/michalueu-examples/tree/master/flickr-michalueu)
+* Android WebView - how to run JavaScript to hide div
+ 1. [__webview-js-android-michalueu__](https://github.com/fockeRR/michalueu-examples/tree/master/webview_js)
---
+++ webview_js/src/eu/michalu/WebViewActivity.java
@@ -0,0 +1,40 @@
+package eu.michalu;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class WebViewActivity extends Activity {
+	WebView mWebView;
+	/*
+	 * array of div's ids to disable/hide
+	 */
+	String[] idsToHide = { "section1", "section3", "section5" };
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		mWebView = (WebView) findViewById(R.id.activity_webview);
+		//enable javascript engine
+		mWebView.getSettings().setJavaScriptEnabled(true);
+		mWebView.setWebViewClient(new WebViewClient() {
+			@Override
+			public void onPageFinished(WebView view, String url) {
+				// TODO Auto-generated method stub
+				super.onPageFinished(view, url);
+				
+				//run 'disableSection' for all divs to hide/disable
+				for (String s : idsToHide) {
+					String surveyId = s;
+					view.loadUrl("javascript:disableSection('" + surveyId + "');");
+				}
+			}
+		});
+		//load webpage from assets
+		mWebView.loadUrl("file:///android_asset/list.html");
+	}
+}
